83fc8fe498dce90c14cdc0fb04ef96f9286c762f
unknown <wanguilamya45@gmail.com>
2023-11-24 18:41:07 +0300

    first commit

---
+++ Book.java
@@ -0,0 +1,9 @@
+ public void main(String[] args) {
+                System.out.println("Title: " = Mirrors Unfold);
+                System.out.println("Author: " = Lamya wangui);
+                System.out.println("Publisher: " = book.HarperCollins Publishers);
+                System.out.println("ISBN: " = 978-3-16-146784-0);
+                System.out.println("Year Published: " = 2009);
+
+    }
+}
